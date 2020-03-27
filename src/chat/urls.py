f001d9924f85e1673de8b2a7dfb9cfbbd2988241
yasin <yasin.kbas12@gmail.com>
2020-03-27 22:28:37 +0300

    add missing forward slash

--- src/chat/urls.py
+++ src/chat/urls.py
@@ -6,5 +6,5 @@ from .views import ThreadView, InboxView
 app_name = 'chat'
 urlpatterns = [
     path("", InboxView.as_view()),
-    re_path(r"^(?P<username>[\w.@+-]+)", ThreadView.as_view()),
+    re_path(r"^(?P<username>[\w.@+-]+)/", ThreadView.as_view()),
 ]
